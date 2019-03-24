06657bd818b32b3902181327597c6004ad7b62dd
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-03-24 16:01:21 +0200

    toggle background on click

--- events/app.js
+++ events/app.js
@@ -1,8 +1,11 @@
-function sayHello(event){
+function onElementClick(event){
+    event.stopPropagation();
+
+    toggleBackground(event.target);
 
     console.log(event);
-    alert('Hell from function');
-    this.removeEventListener('click', sayHello);
+    // alert('Hell from function');
+    // this.removeEventListener('click', sayHello);
 }
 
 const h2 = 
@@ -10,7 +13,16 @@ const h2 =
 
 h2.addEventListener('click', sayHello);
 
-document.getElementById('title')
-    .addEventListener('click', sayHello);
+document.body.addEventListener('click', function(e){
+    console.log('body', e);
 
-    
+    toggleBackground(e.target);
+
+})
+
+function toggleBackground(el){
+    el.style.background =
+        el.style.background === 'yellow' ?
+            'red':'yellow';
+}
+
