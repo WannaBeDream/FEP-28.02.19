1e7e1ecd9aaf81a86b998d451f0c0620971d5eb6
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-04-06 11:26:29 +0300

    Add Ball Game task

---
+++ ball-game/app.js
@@ -0,0 +1,11 @@
+function BallGame(el, height, widtg){
+
+}
+
+const game = new BallGame(
+                        document.getElementById('container'),
+                        400,
+                        400
+                    );
+
+game.init();
