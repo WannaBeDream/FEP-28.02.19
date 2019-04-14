027ec58b762246380f9a5c62254ed15b6d2b8e8a
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-04-14 11:49:38 +0300

    Add Accordeon implementation

--- accordeon/app.js
+++ accordeon/app.js
@@ -1,11 +1,67 @@
-function Accordion(el, config){
+function Accordeon(el, config){
+    this.container = el;
+    this.config = config;
+
+    this.init();
 }
 
-const accordion = new Accordeon(
+Accordeon.prototype.init = function(){
+    this.bindEventListener();
+}
+
+Accordeon.prototype.bindEventListener = function(){
+    this.container.addEventListener('click', (e) => this.onContainerClick(e));
+}
+
+Accordeon.prototype.onContainerClick = function(e) {
+    if (e.target.classList.contains('accordeon-heading')){
+        this.toggleElement(e.target.parentNode);
+    }
+}
+
+Accordeon.prototype.toggleElement = function(el) {
+    if (el.classList.contains('open')){
+        this.closeElement(el);
+    } else {
+        this.openElement(el);
+    }
+}
+
+Accordeon.prototype.closeElement = function(el){
+    console.log(el);
+    el.classList.remove('open');
+}
+
+Accordeon.prototype.closeAllElements = function(){
+    Array.prototype.forEach.call(this.container.children, this.closeElement);
+}
+
+Accordeon.prototype.openElement = function(el){
+    if (this.config.collapseOther){
+        this.closeAllElements();
+    }
+
+    el.classList.add('open');
+}
+
+Accordeon.prototype.open = function(index){
+    this.openElement(this.container.children[index]);
+}
+
+Accordeon.prototype.close = function(index){
+    this.closeElement(this.container.children[index]);
+}
+
+Accordeon.prototype.toggle = function(index){
+    this.toggleElement(this.container.children[index]);
+}
+
+const acc = new Accordeon(
                         document.getElementById('container'),
                         {collapseOther: true}
                     );
 
-accordion.open(0);
-accordion.close(0);
-accordion.toggle(1);
+acc.open(0);
+setTimeout(()=> acc.toggle(1), 3000);
+// acc.close(0);
+// acc.toggle(1);
