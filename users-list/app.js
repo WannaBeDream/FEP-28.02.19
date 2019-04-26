b98214ac8a16bbff3142f3dca447a49398d29d07
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-04-26 11:42:51 +0300

    Add request function

--- users-list/app.js
+++ users-list/app.js
@@ -5,3 +5,20 @@ class Users{
 const usersList = new Users(
     document.getElementById('usersListTable')
 )
+
+
+/****** Function to make request *******
+USAGE: 
+request('get', 'http://exmaple.com', (resp)=> {console.log(resp)} );
+*/
+
+var request = function() {
+    var xhr = new XMLHttpRequest();
+    return function( method, url, callback ) {
+        xhr.onload = function() {
+            callback( JSON.parse(xhr.responseText));
+        };
+        xhr.open( method, url );
+        xhr.send();
+    };
+}();
