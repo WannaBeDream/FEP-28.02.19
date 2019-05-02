5f609f8c582ed199c64a47a156e78d41a97df4be
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-05-02 13:25:36 +0300

    Users list implementation

--- users-list/app.js
+++ users-list/app.js
@@ -1,11 +1,3 @@
-class Users{
-
-}
-
-const usersList = new Users(
-    document.getElementById('usersListTable')
-)
-
 
 /****** Function to make request *******
 USAGE: 
@@ -22,3 +14,57 @@ var request = function() {
         xhr.send();
     };
 }();
+class Users{
+    static BASE_URL = 'https://jsonplaceholder.typicode.com';
+    static USERS_PATH = '/users';
+    static POSTS_PATH = '/posts';
+    static USER_ROW_TEMPLATE = document.getElementById('userTemplate').innerHTML;
+
+    constructor(container){
+        this.container = container;
+        this.init();
+    }
+
+    init(){
+        this.onBodyClick = this.onBodyClick.bind(this);
+        this.renderUsers = this.renderUsers.bind(this);
+
+        this.tbody = this.container.getElementsByTagName('tbody')[0];
+        this.tbody.addEventListener('click', this.onBodyClick)
+        this.fetchUsers();
+    }
+
+    onBodyClick(e){
+        const id = e.target.parentNode.dataset.userId;
+
+        this.fetchUserPosts(id);
+    }
+
+    fetchUsers(){
+        request('get', Users.BASE_URL + Users.USERS_PATH, this.renderUsers)
+    }
+
+    renderUsers(usersList){
+        this.tbody.innerHTML = usersList.map((user) => {
+            return Users.USER_ROW_TEMPLATE
+                .replace('{{id}}', user.id)
+                .replace('{{name}}', user.name)
+                .replace('{{phone}}', user.phone)
+                .replace('{{email}}', user.email)
+        }).join('\n');
+    }
+
+    fetchUserPosts(userId){
+        const url = Users.BASE_URL + Users.POSTS_PATH + '?userId=' + userId;
+        request('get', url, this.renderUserPosts)
+    }
+
+    renderUserPosts(posts){
+        console.log(posts);
+    }
+
+}
+
+const usersList = new Users(
+    document.getElementById('usersListTable')
+)
