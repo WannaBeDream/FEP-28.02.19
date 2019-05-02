9c5c99455ca6de3a6977f39669fbfc4f4f2bb2b8
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-05-02 17:33:56 +0300

    Change var to const

--- users-list/app.js
+++ users-list/app.js
@@ -4,8 +4,8 @@ USAGE:
 request('get', 'http://exmaple.com', (resp)=> {console.log(resp)} );
 */
 
-var request = function() {
-    var xhr = new XMLHttpRequest();
+const request = function() {
+    const xhr = new XMLHttpRequest();
     return function( method, url, callback ) {
         xhr.onload = function() {
             callback( JSON.parse(xhr.responseText));
@@ -68,3 +68,4 @@ class Users{
 const usersList = new Users(
     document.getElementById('usersListTable')
 )
+
